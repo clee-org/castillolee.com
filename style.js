e76196f4367ac550ec2501ae0c25202de4aad866
LevelAndre <84067630+LevelAndre@users.noreply.github.com>
2022-06-16 00:11:11 -0700

    Update style.js

--- style.js
+++ style.js
@@ -1,7 +1,36 @@
-    function show(show, hidden)
-    {
-        document.getElementById(shown).style.display='block';
-        document.getElementById(hidden).style.display='none';
-        return false;
+    window.onscroll = function() {headerMenu()};
+
+    var navbar = document.getElementById("navbar");
+
+    var sticky = navbar.offsetTop;
+
+    function headerMenu() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        }
+        else {
+            navbar.clasList.remove("sticky");
+        }
+    }
+
+    function buttonAnimation(x) {
+        x.classList.toggle("change");
+
+    }
+    function clickyMenu() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onClick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementByClassName("dropdown-content");
+            var i;
+            for (i=0; i<dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
     }
 
