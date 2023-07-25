39020320ac9a72cd518991075e2fcc2ac298c507
LevelAndre <andrelee089@gmail.com>
2023-07-25 03:34:35 -0700

    Version 2.0

---
+++ Homestyle.js
@@ -0,0 +1,9 @@
+function instance() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    }
+    else {
+        x.className = "nav";
+    }
+}
